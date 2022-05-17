29dd93a2e53f854d56b8deeb6a6862d2ae275045
github.com/SoftPathTechnologies <promiseobe531@gmail.com>
2022-05-17 13:52:23 +0100

    Project Initialization

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:furniture_shop/components/product_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Funiture Shop',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ProductScreen(),
+    );
+  }
+}
