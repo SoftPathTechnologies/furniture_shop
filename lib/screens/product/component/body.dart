57eebf19031871c0f916f6bcc43b7faff5e67950
github.com/SoftPathTechnologies <promiseobe531@gmail.com>
2022-05-18 14:42:47 +0100

    Updated Product Cards

--- lib/screens/product/component/body.dart
+++ lib/screens/product/component/body.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:furniture_shop/components/search_box.dart';
 import 'package:furniture_shop/constants.dart';
+import 'package:furniture_shop/models/product.dart';
 import 'package:furniture_shop/screens/product/component/category_list.dart';
+import 'package:furniture_shop/screens/product/component/product_card.dart';
 
 class Body extends StatelessWidget {
   const Body({Key? key}) : super(key: key);
@@ -28,90 +30,15 @@ class Body extends StatelessWidget {
                       topLeft: Radius.circular(40),
                       topRight: Radius.circular(40))),
             ),
-            productCard(),
+            ListView.builder(
+                itemCount: products.length,
+                itemBuilder: (context, index) => productCard(
+                      itemIndex: index,
+                      product: products[index],
+                    )),
           ],
         ))
       ],
-    );
-  }
-}
-
-class productCard extends StatelessWidget {
-  const productCard({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Container(
-      height: 160,
-      margin: EdgeInsets.symmetric(
-          horizontal: kDefaultPadding, vertical: kDefaultPadding / 2),
-      // color: Colors.blueAccent,
-      child: Stack(
-        alignment: Alignment.bottomCenter,
-        children: <Widget>[
-          Container(
-            height: 136,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                color: kBlueColor,
-                boxShadow: [kDefaultShadow]),
-            child: Container(
-              margin: EdgeInsets.only(right: 10),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20), color: Colors.white),
-            ),
-          ),
-          Positioned(
-              top: 0,
-              right: 0,
-              child: Container(
-                padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                height: 160,
-                width: 200,
-                child: Image.asset(
-                  "assets/images/Item_1.png",
-                  fit: BoxFit.cover,
-                ),
-              )),
-          // Product Title and Price
-          Positioned(
-              bottom: 0,
-              left: 0,
-              child: SizedBox(
-                height: 136,
-                width: size.width - 200,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: kDefaultPadding),
-                      child: Text(
-                        "Classic Leather Arm Chair",
-                        style: Theme.of(context).textTheme.button,
-                      ),
-                    ),
-                    Spacer(),
-                    Container(
-                      padding: EdgeInsets.symmetric(
-                          horizontal: kDefaultPadding * 1.5,
-                          vertical: kDefaultPadding / 4),
-                      decoration: BoxDecoration(
-                          color: kSecondaryColor,
-                          borderRadius: BorderRadius.only(
-                              bottomLeft: Radius.circular(20),
-                              topRight: Radius.circular(20))),
-                      child: Text("\$ 90.00",
-                          style: Theme.of(context).textTheme.button),
-                    )
-                  ],
-                ),
-              ))
-        ],
-      ),
     );
   }
 }
